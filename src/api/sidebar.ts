9ff6a7d76beec5b59e247344fb7ac47fb7c54bbc
Saugat <saugat.gautam2002@gmail.com>
2024-09-03 23:15:18 +0545

    Sidebar Setup

---
+++ src/api/sidebar.ts
@@ -0,0 +1,45 @@
+export const siderbaritems = [
+  {
+    heading: "Administration",
+    sidebaritems: [
+      {
+        label: "Dashboard",
+        icon: "",
+      },
+
+      {
+        label: "Members",
+        icon: "",
+      },
+    ],
+  },
+  {
+    heading: "Management",
+    sidebaritems: [
+      {
+        label: "Request",
+        icon: "",
+      },
+
+      {
+        label: "Invoices",
+        icon: "",
+      },
+      {
+        label: "Transaction",
+        icon: "",
+      },
+    ],
+  },
+
+  {
+    heading: "Accounting",
+    sidebaritems: [
+      {
+        label: "Reports",
+        icon: "",
+      },
+
+    ]
+  }
+];
--- src/app/components/ui/sidebar/sideBar.tsx
+++ src/app/components/ui/sidebar/sideBar.tsx
@@ -1,25 +1,72 @@
 import Image from "next/image";
 import React from "react";
 import Line from "./line/line";
+import { siderbaritems } from "@/api/sidebar";
 
 const SideBar = () => {
   return (
-    <div className="bg-light rounded-lg px-8 pt-[40px] pb-6 flex flex-col items-center gap-[40px]">
-      <div className="flex flex-col items-center">
-        <div className="w-[64px] h-[64px] ">
+    // <div className="bg-light rounded-lg px-8 pt-[40px] pb-6 flex flex-col items-center gap-[40px]">
+    //   <div className="flex flex-col items-center">
+    //     <div className="w-[64px] h-[64px] ">
+    //       <Image
+    //         src="/profile/pp.png"
+    //         width={100}
+    //         height={100}
+    //         className="h-full w-full rounded-xl object-cover"
+    //         alt="profilePicture"
+    //       />
+    //     </div>
+    //     <div className="text-white text-md text-center  ">Your Name</div>
+    //   </div>
+    //   <Line />
+    //   <div>Profile</div>
+    //   <div>Profile</div>
+    //   <div>Profile</div>
+    // </div>
+
+    <div className="">
+      <section className="flex gap-5 flex-col items-center">
+        <div>
           <Image
             src="/profile/pp.png"
             width={100}
             height={100}
-            className="h-full w-full rounded-xl object-cover"
-            alt="profilePicture"
+            alt="Main Image"
           />
         </div>
-        <div className="text-white text-md text-center  ">Your Name</div>
-      </div>
-      <Line />
-      <div>Profile</div>
-      <div>Profile</div>
+
+        <p className="text-white "> Hi, Mary</p>
+      </section>
+
+        <div className="py-6">
+          <Line />
+        </div>
+
+      <section className="text-white">
+       
+       {
+        siderbaritems.map ((sidebarheading,index) => {
+          return (
+            <div>
+            
+              {sidebarheading.heading}
+
+              {
+                sidebarheading.sidebaritems.map((sidebaritems,index) => (
+                  <div>
+                    {sidebaritems.label}
+                  </div>
+                )
+              )
+              }
+            </div>
+          )
+        }
+      )
+       }
+
+        
+      </section>
     </div>
   );
 };
